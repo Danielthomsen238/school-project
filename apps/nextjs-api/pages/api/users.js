912ca23d3d3a8701d5435c68b2e30baf3e7dadbb
Optic-Dex <danielthomsen238@gmail.com>
2022-08-30 12:47:10 +0200

    access token

--- apps/nextjs-api/pages/api/users.js
+++ apps/nextjs-api/pages/api/users.js
@@ -1,11 +1,21 @@
 import excuteQuery from "../../src/db";
+import NextCors from "nextjs-cors"
 
 
 async function handler(req, res) {
+      // Run the cors middleware
+   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+   await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   })
+
 if(req.method === "DELETE"){
     const {id} = req.body
     if(!id){
-        req.status(422).json({message: "Invalid Data"})
+        res.status(422).json({message: "Invalid Data"})
         return
     }
     await excuteQuery(`DELETE FROM users WHERE id = '${id}'`)
@@ -15,8 +25,8 @@ if(req.method === "DELETE"){
 if(req.method === "POST"){
     console.log(req.body)
     const {username, password, email} = req.body
-    if(!username || !password || !email){
-        req.status(422).json({message: "Invalid Data"})
+    if(!username || !password || !email ){
+        res.status(422).json({message: "Invalid Data"})
         return
     }
     const data = await excuteQuery(`INSERT INTO users(username, password, email) VALUES('${username}', '${password}', '${email}')`)
@@ -25,9 +35,20 @@ if(req.method === "POST"){
 
 }    
 if(req.method === "GET"){
-    const data = await excuteQuery(`SELECT * from users`)
-    res.status(201).json({message: "data fetched", data})
-    return
+    const { token, id } = req.body 
+    if(!token|| !id ) {
+        res.status(422).json({message: "No token or id, access denied"})
+        return
+    }
+    const accessToken = await excuteQuery(`SELECT api_token from tokens WHERE user_id = '${id}'`)
+    if(token !== accessToken[0].api_token){
+       res.status(422).json({message: "access denied"})
+    
+    }else if(token === accessToken[0].api_token){
+        const data = await excuteQuery(`SELECT * from users`)
+        res.status(201).json({message: "data fetched", data})
+        return
+    }
 }
 else {
     res.status(500).json({message: "Route not valid"})
