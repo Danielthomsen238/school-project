d482959735ba792f416074e078bd83bdb3ec26b6
Optic-Dex <danielthomsen238@gmail.com>
2022-09-01 14:17:57 +0200

    hello pwa

--- apps/to-do-list/next.config.js
+++ apps/to-do-list/next.config.js
@@ -1,18 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const withNx = require('@nrwl/next/plugins/with-nx');
+const withNx = require('@nrwl/next/plugins/with-nx');
 const withPWA  = require("next-pwa");
 
-// @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
-//  **/
-// const nextConfig = {
-//   nx: {
-//     // Set this to true if you would like to to use SVGR
-//     // See: https://github.com/gregberge/svgr
-//     svgr: false,
-//   },
-// };
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+};
 
-// module.exports = withNx(nextConfig) 
+
+module.exports = withNx(nextConfig)
 module.exports = withPWA({
   //...before
    pwa: {
@@ -21,4 +23,5 @@ module.exports = withPWA({
      skipWaiting: true,
    },
    //...after
- }) 
+ });
+ 
